1db4fbb76cc5e0218958257bac3d0afb8de9e9f3
Catalin Albu <cataablu1919@gmail.com>
2023-02-14 22:27:15 +0200

    Create a basic store with a counter and dispatch actions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const [value, setValue] = useState();
+  const dispatch = useDispatch();
+  const handleInc = () => {
+    dispatch({ type: 'INC' });
+  };
+  const handleDec = () => {
+    dispatch({ type: 'DEC' });
+  };
+  const handleAdd = () => {
+    dispatch({ type: 'ADD', payload: value });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>App counter</h1>
+      <h2>{counter}</h2>
+      <button onClick={handleInc}>Increment</button>
+      <button onClick={handleDec}>Decrement</button>
+      <input type="number" onChange={(e) => setValue(e.target.value)} />
+      <button onClick={handleAdd}>Add</button>
     </div>
   );
 }
---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import { createStore } from 'redux';
+
+const reducerFn = (state = { counter: 0 }, action) => {
+  switch (action.type) {
+    case 'INC':
+      return { counter: state.counter + 1 };
+    case 'DEC':
+      return { counter: state.counter - 1 };
+    case 'ADD':
+      return { counter: state.counter + Number.parseInt(action.payload) };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducerFn);
+
+export default store;
